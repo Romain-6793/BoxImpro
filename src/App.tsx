b6d74c55a6cf13acd2f5baa8bd3a86167d223148
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2023-06-07 21:50:05 +0200

    primereact header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import "primereact/resources/themes/vela-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Specialists from "./pages/Specialists/Specialists";
 import Exercices from "./pages/Exercices/Exercices";
 
-import "./App.css";
-
 function App() {
   // Typer le routeur
 
   return (
-    <div>
+    <div className='App'>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/specialists' element={<Specialists />} />
         <Route path='/exercices' element={<Exercices />} />
       </Routes>
-      <Header />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,65 +1,76 @@
+// import { Link } from "react-router-dom";
+import { Menubar } from "primereact/menubar";
+// import styled from "styled-components";
+// import BoxImproLogo from "../../assets/boximpro-logo.png";
 
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import BoxImproLogo from '../../assets/boximpro-logo.png'
+// const HomeLogoContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   @media (max-width: 425px) {
+//     margin-right: 10px;
+//   }
+// `;
 
+// const HomeLogo = styled.img`
+//   width: 100px;
+//   height: 100px;
+// `;
 
-const StyledHeader = styled.header`
-width: 100%;
-`
-
-const NavContainer = styled.nav`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 5px 20px;
-`
-
-const HomeLogoContainer = styled.div`
-display: flex;
-align-items: center;
-@media (max-width: 425px) {
-  margin-right: 10px;
-}
-`
-
-const HomeLogo = styled.img`
- width: 100px;
- height: 100px;
-`
-
-const StyledLink = styled(Link)`
-margin-right: 0.5rem;
-// text-decoration: none;
-font-weight: bold;
-font-size: 18px;
-@media (max-width: 767px) {
-  font-size: 10px;
-}
-`
+// const StyledLink = styled(Link)`
+//   margin-right: 0.5rem;
+//   // text-decoration: none;
+//   font-weight: bold;
+//   font-size: 18px;
+//   @media (max-width: 767px) {
+//     font-size: 10px;
+//   }
+// `;
 
 function Header() {
+  // Typer les props de lien
 
-  // Typer les props de lien 
+  const navlist = [
+    {
+      label: "Accueil",
+      icon: "pi pi-fw pi-home",
+      command: () => {
+        window.location.href = "/";
+      },
+    },
+    {
+      label: "Catégories",
+      icon: "pi pi-fw pi-bookmark",
+      command: () => {
+        window.location.href = "/specialists";
+      },
+    },
+    {
+      label: "Exercices",
+      icon: "pi pi-fw pi-bookmark",
+      command: () => {
+        window.location.href = "/exercices";
+      },
+    },
+  ];
 
- return (
-  <StyledHeader>
-    <NavContainer>
-    <Link to="/">
-      <HomeLogoContainer>
-      <HomeLogo src={BoxImproLogo} />
-      </HomeLogoContainer>
-    </Link>
-      <StyledLink to="/specialists">
-        Catégories d'impro
-      </StyledLink>
-      <StyledLink to="/exercices">
-        Exercices d'impro
-      </StyledLink>
-    </NavContainer>
-  </StyledHeader>
-  
- )
+  return (
+    <div>
+      <header>
+        <nav>
+          {/* <Link to='/'>
+            <HomeLogoContainer>
+              <HomeLogo src={BoxImproLogo} />
+            </HomeLogoContainer>
+          </Link>
+          <StyledLink to='/specialists'>Catégories d'impro</StyledLink>
+          <StyledLink to='/exercices'>Exercices d'impro</StyledLink> */}
+          <ul>
+            <Menubar model={navlist} />
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
