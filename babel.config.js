a5b1d8cbf6e6193eca8bd6f5a74aa9fd8f829336
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2023-09-05 14:22:20 +0200

    testing config

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+// Created to match jest with TypeScript
+
+module.exports = {
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }], "@babel/preset-typescript"],
+};
+
---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+// };
+
+// import type { Config } from 'jest';
+
+// const config: Config = {
+//   verbose: true,
+// };
+
+// export default config;
+
+// import type { Config } from 'jest';
+// import { defaults } from 'jest-config';
+
+// const config: Config = {
+//   moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+// };
+
+// export default config;
